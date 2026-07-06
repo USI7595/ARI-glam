006f323afce62d9b461ea7d612c7e09c55da5e15
USI7595 <lawalusman2003@gmail.com>
2026-07-06 13:00:02 -0700

    web icon

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,16 @@
+import Head from "next/head";
 import "../src/styles.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/assets/ari-glam-box.jpeg" type="image/jpeg" />
+        <link rel="shortcut icon" href="/assets/ari-glam-box.jpeg" type="image/jpeg" />
+        <title>ARI Glam Makeup Artistry</title>
+        <meta name="description" content="Soft glam, bridal beauty, and event-ready makeup with a polished finish made for your moment." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
